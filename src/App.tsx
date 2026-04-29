edaf1103631ed3a878deabf4996fb0d940c5a7ed
Hafida Belayd <hafidabelaidagnaoui@gmail.com>
2026-04-29 18:48:45 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1829,7 +1829,6 @@ Reste concis, technique et professionnel. Signe l'analyse par "Généré par Fus
                         </div>
                       </div>
                     </div>
-                  </Card>
                 </div>
               </div>
 
